208d465810e349c6406a63b3904224f6b12766a1
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-12-29 21:02:12 +0000

    hi

--- src/components/Drag-Drop/drop-zone.js
+++ src/components/Drag-Drop/drop-zone.js
@@ -15,6 +15,7 @@ class DropZone extends React.Component {
   handleDrop(e){
     try{
       let data = JSON.parse(e.dataTransfer.getData('application/json'));
+      console.log(data, this.props.id);
       this.props.onComplete(data, this.props.id);
     }
     catch(e){
